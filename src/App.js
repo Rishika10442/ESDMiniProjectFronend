085f8af5535a8fbb3c711840006f694344382936
Rishika10442 <10442rishika@gmail.com>
2024-11-27 22:55:58 +0530

    frontend code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const onLoginSuccess = (data) => {
+  console.log("Login successful:", data);
+
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div><Login onLoginSuccess={onLoginSuccess} /></div>,
+  },
+  {
+    path: "/dashboard",
+    element: <div><Dashboard/></div>
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}/>
     </div>
   );
 }
